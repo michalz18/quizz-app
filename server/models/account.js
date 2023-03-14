ee67c029590c30ee4b129a2fd2ac59855a7c82d2
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2023-03-14 12:54:35 +0100

    datbases have examples

--- server/models/account.js
+++ server/models/account.js
@@ -4,6 +4,7 @@ const { Schema, model } = mongoose;
 const accountSchema = new Schema({
   email: String,
   password: String,
+  quizes: Array
 });
 
 const account = model("account", accountSchema);
