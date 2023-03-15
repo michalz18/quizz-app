b0657dcdb7c651e49ba297d388f1e825a3508e2a
Artur Pokora <apokora@itsap.pl>
2023-03-15 11:40:56 +0100

    remove bgc from h2 tag for home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,13 +6,15 @@ export default function Home({ goToQuizes }) {
 	return (
 		<div className='Home'>
 			<div>
-				<h2>Test your trivia with QuizApp</h2>
+				<h2 className='HomeTitle'>Test your trivia with QuizApp</h2>
 			</div>
 			<img className='HomeImg' src={img} alt='' />
-			<div className="HomeText">
+			<div className='HomeText'>
 				<p>Boost your brainpower with our quizzes</p>
 			</div>
-			<button className="HomeButton" onClick={goToQuizes}>Start solving</button>
+			<button className='HomeButton' onClick={goToQuizes}>
+				Start solving
+			</button>
 		</div>
 	)
 }
