e6bdc4d8905ab5eed654edbccc8f33f775c45a62
Michał Zuzak <michalzuzak@MacBook-Pro.local>
2023-03-27 13:25:58 +0200

    Logout featuer and use context added

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,40 +1,47 @@
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import "./Header.css";
 import MenuChoice from "./MenuChoice.js";
-import { useEffect, useState } from 'react';
-import logo from './logo.jpg'
-import DropdownMenu from "./DropdownMenu"
+import logo from "./logo.jpg";
+import DropdownMenu from "./DropdownMenu";
+import { useLoggedUser } from "../../App";
 
-
-function Header({ menuChoices, changePage, onLogoClick, openModal, loggedUser }) {
+function Header({ menuChoices, changePage, onLogoClick, openModal }) {
+  const { loggedUser } = useLoggedUser();
   const [choices, setChoices] = useState([]);
+
   useEffect(() => {
-    setChoices(menuChoices.filter(choice => choice.text != 'Home'))
-  }, [])
-  
+    setChoices(menuChoices.filter((choice) => choice.text !== "Home"));
+  }, []);
+
   return (
     <div id="header">
       <div id="logo-and-name-wrapper" onClick={onLogoClick}>
         <div id="logo-wrapper">
-          <img className='HeaderLogo' id="logo" src={logo} alt="logo"></img>
+          <img className="HeaderLogo" id="logo" src={logo} alt="logo"></img>
         </div>
-        <div id="app-name">Quiz<section id="sec-part">App</section></div>
+        <div id="app-name">
+          Quiz<section id="sec-part">App</section>
+        </div>
       </div>
       <div id="navigation">
         <div id="navbar-wrapper">
           <div id="navbar">
-            {choices.map((menuChoice, index) => 
-            <MenuChoice key={index} menuChoice={menuChoice} changePage={changePage}/>
-            )}
+            {choices.map((menuChoice, index) => (
+              <MenuChoice key={index} menuChoice={menuChoice} changePage={changePage} />
+            ))}
           </div>
         </div>
         <div id="bar-icon-wrapper">
           <img id="bar-icon" src="/menu.png" alt="menu-icon"></img>
         </div>
       </div>
-      {loggedUser.length === 0
-        ? <button id="login" onClick={openModal}>LOGIN</button>
-        : <DropdownMenu changePage={changePage}/>
-        }
+      {loggedUser.length === 0 ? (
+        <button id="login" onClick={openModal}>
+          LOGIN
+        </button>
+      ) : (
+        <DropdownMenu changePage={changePage} />
+      )}
     </div>
   );
 }
